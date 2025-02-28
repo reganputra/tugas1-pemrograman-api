ebea4cf52dd691e128961610ccf2e82e3a4e8377
reganputra <ramadhanregan334@gmail.com>
2025-02-28 22:24:07 +0700

    refactor: error message

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let usersList = []; // menyimpan list dari GET agar bisa dimanipulasi
 async function fetchData(url, options = {}) {
     try {
         const response = await fetch(url, options);
-        if (!response.ok) throw new Error("Network response was not ok");
+        if (!response.ok) throw new Error("Internal Server Error");
         return await response.json();
     } catch (error) {
         console.error("Fetch error:", error);
